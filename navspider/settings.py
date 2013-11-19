dfc72a3c77442eeb94343705b03cd8ba1422ab78
dolfly <dolfly@foxmail.com>
2013-11-19 15:57:29 +0800

    * fix

--- navspider/settings.py
+++ navspider/settings.py
@@ -1,4 +1,4 @@
-# Scrapy settings for spider project
+# Scrapy settings for navspider project
 #
 # For simplicity, this file contains only the most important settings by
 # default. All the other settings are documented here:
@@ -6,10 +6,10 @@
 #     http://doc.scrapy.org/en/latest/topics/settings.html
 #
 
-BOT_NAME = 'spider'
+BOT_NAME = 'navspider'
 
-SPIDER_MODULES = ['spider.spiders']
-NEWSPIDER_MODULE = 'spider.spiders'
+SPIDER_MODULES = ['navspider.spiders']
+NEWSPIDER_MODULE = 'navspider.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'spider (+http://www.yourdomain.com)'
+#USER_AGENT = 'navspider (+http://www.yourdomain.com)'
